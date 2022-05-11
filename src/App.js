4d506d6f913e146c589748c1ad13bf91e0a3b8b1
Md.Minhajul Islam <31633249+differentcoder@users.noreply.github.com>
2022-05-11 13:06:08 +0600

    Website pages Route All

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/Home/About';
 import Navbar from './Pages/Shared/Navbar';
+import Login from './Pages/Home/Login';
+import Appointment from './Pages/Home/Appointment';
+import Review from './Pages/Home/Review';
+import Contact from './Pages/Home/Contact';
 function App() {
   return (
     <div>
@@ -10,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login/>} />
+        <Route path="appointment" element={<Appointment/>} />
+        <Route path="review" element={<Review/>} />
+        <Route path="contact" element={<Contact/>} />
       </Routes>
     </div>
   );
