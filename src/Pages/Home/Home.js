dc30e6e34da3bd04ba12c51eb352174fd0e1bc40
Md.Minhajul Islam <31633249+differentcoder@users.noreply.github.com>
2022-05-12 18:00:00 +0600

    MakeAppointment update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import CallToAction from './CallToAction';
 import Info from './Info';
+import MakeAppointment from './MakeAppointment';
 import Services from './Services';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Info></Info>
             <Services></Services>
             <CallToAction></CallToAction>
+            <MakeAppointment></MakeAppointment>
         </div>
     );
 };
