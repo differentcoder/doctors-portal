f673e5de52e4db4efe4f8d0f21e0a731c25305ec
Md.Minhajul Islam <31633249+differentcoder@users.noreply.github.com>
2022-05-21 19:18:03 +0600

    Google Firebase Auth

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,8 +4,8 @@ const Service = ({service,setTreatment}) => {
     const {name,slots} = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+            <div class="card-body text-center">
+                <h2 class="text-xl font-bold text-secondary">{name}</h2>
                 <p>
                     {
                         slots.length > 0
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    if (user) {
+       
+    }
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="flex justify-center items-center h-screen">
+            <div className="card bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-blod">Login</h2>
+                   
+                    <div className="divider">OR</div>
+                    <button
+                     onClick={() => signInWithGoogle()}
+                      className="btn btn-outline ">
+                           Continue with Google
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
