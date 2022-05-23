d26401d911dbb6b14a3f689b10ed29ef1c2bfa83
Md.Minhajul Islam <31633249+differentcoder@users.noreply.github.com>
2022-05-23 11:57:30 +0600

    Booking user data update

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,39 +1,43 @@
-import { format } from 'date-fns';
 import React from 'react';
+import { format } from 'date-fns';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
-const BookingModal = ({date,treatment,setTreatment}) => {
-    const {_id, name,slots} = treatment;
+const BookingModal = ({ date, treatment, setTreatment }) => {
+    const { _id, name, slots } = treatment;
+    const [user, loading, error] = useAuthState(auth);
 
     const handleBooking = event =>{
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(slot);
+        console.log(_id, name, slot);
+        
+        // to close the modal
         setTreatment(null);
     }
+
     return (
         <div>
-    
-            {/* <label for="booking-modal" className="btn modal-button">open modal</label> */}
-
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
-                <div className="modal-box text-center">
-                <label for="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                  
-                    <h3 className="font-bold text-lg">Booking For : {name}</h3>
-                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-2 justify-center'>
+                <div className="modal-box">
+                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="font-bold text-lg text-secondary">Booking for: {name}</h3>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
                         <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
-                        <select name="slot" className='select select-bordered w-full max-w-xs'>
+                        <select name="slot" className="select select-bordered w-full max-w-xs">
                             {
-                                slots.map(slot => <option value={slot}>{slot}</option>)
+                                slots.map((slot, index) =><option 
+                                    key={index}
+                                    value={slot}
+                                >{slot}</option>)
                             }
                         </select>
-                        <input type="text"  name ="name" placeholder="Your Name here" className="input input-bordered w-full max-w-xs" />
-                        <input type="email"  name ="email"  placeholder="Email here" className="input input-bordered w-full max-w-xs" />
-                        <input type="text" name="phone" placeholder="Your Phone here" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="name" disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name="email" disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
-                   
                 </div>
             </div>
         </div>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -16,6 +16,7 @@ const Navbar = () => {
         <li><Link to="/review">Review</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li>{user? <button className='btn btn-ghost' onClick={logout}>Sign out</button>:<Link to="/login">Login</Link>}</li>
+        {/* <li>{user?.displayName || ''}<br></br>{user?.email || ''}</li> */}
     </>
     return (
         <div className="navbar bg-base-100">
