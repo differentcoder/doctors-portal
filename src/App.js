0fac58d3d03ab97ae6cfcfc26e58bdc4e2c5a7f6
Md.Minhajul Islam <31633249+differentcoder@users.noreply.github.com>
2022-05-23 11:51:34 +0600

    Login , Signup & Require file update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/Home/About';
 import Navbar from './Pages/Shared/Navbar';
-import Review from './Pages/Home/Review';
+// import Review from './Pages/Home/Review';
 import Contact from './Pages/Home/Contact';
 import Login from './Pages/Login/Login';
 import Appointment from './Pages/Appointment/Appointment'
+import SignUp from './Pages/Login/SignUp';
+import RequireAuth from './Pages/Login/RequireAuth';
 function App() {
   return (
     <div className='max-w-7xl mx-auto'>
@@ -15,8 +17,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login/>} />
-        <Route path="appointment" element={<Appointment/>} />
-        <Route path="review" element={<Review/>} />
+        <Route path="signup" element={<SignUp/>} />
+        <Route path="appointment" element={
+          <RequireAuth>
+            <Appointment></Appointment>
+          </RequireAuth>
+        } />
+        {/* <Route path="review" element={<Review/>} /> */}
         <Route path="contact" element={<Contact/>} />
       </Routes>
     </div>
